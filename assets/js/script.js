f549c5c472eee3741512a587d32adadb25dcf949
Griffin Gore <griffingore@Griffins-MacBook-Pro.local>
2023-06-23 10:40:51 -0700

    inital js and css added

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('#menu a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
