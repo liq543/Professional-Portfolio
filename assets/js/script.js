3b9f3156a8a05c1c8bd4ddeb69c296e0c99d0002
Griffin Gore <griffingore@Griffins-MacBook-Pro.local>
2023-06-23 12:05:53 -0700

    MORE NYAN

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('nyan-button').addEventListener('click', function() {
         var nyanCat = document.getElementById('nyan-cat');
+        var nyanAudio = document.getElementById('nyan-audio');
         nyanCat.style.display = 'block';
+        nyanAudio.play();
 
-        var pos = -771;  // starting position (should match the CSS)
-        var id = setInterval(frame, 1);  // change the second parameter to make the animation faster or slower
+        var pos = -150;  // starting position (should match the CSS)
+        var id = setInterval(frame, 10);  // change the second parameter to make the animation faster or slower
 
         function frame() {
             if (pos == window.innerWidth) {
                 clearInterval(id);
                 nyanCat.style.display = 'none';
+                nyanAudio.pause();
+                nyanAudio.currentTime = 0;
             } else {
                 pos++;
                 nyanCat.style.left = pos + 'px';
