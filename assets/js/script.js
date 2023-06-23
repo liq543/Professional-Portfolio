5f4ab92929b5c0e57665240f8527a138d10ed00d
Griffin Gore <griffingore@Griffins-MacBook-Pro.local>
2023-06-23 12:18:38 -0700

    Nyan Counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,40 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    var nyanSong = new Audio('./assets/audio/nyan.mp3');
+    var nyanCount = 0;
+    var finishedNyanCount = 0;
+    var nyanCounter = document.getElementById('nyan-counter');
+    
     document.getElementById('nyan-button').addEventListener('click', function() {
-        var nyanCat = document.getElementById('nyan-cat');
-        var nyanAudio = document.getElementById('nyan-audio');
-        nyanCat.style.display = 'block';
-        nyanAudio.play();
-
+        var nyanCat = document.createElement('div');
+        nyanCat.classList.add('nyan-cat');
+        nyanCat.innerHTML = '<img src="./assets/img/nyan.png" alt="Nyan Cat" />';
+        document.body.appendChild(nyanCat);
+    
         var pos = -150;  // starting position (should match the CSS)
-        var id = setInterval(frame, 10);  // change the second parameter to make the animation faster or slower
-
+        var id = setInterval(frame, 5);  // change the speed of the animation by changing the second parameter
+    
+        if (nyanCount === 0) {
+            nyanSong.play();
+            nyanCounter.style.opacity = 1;
+        }
+        nyanCount++;
+    
         function frame() {
             if (pos == window.innerWidth) {
                 clearInterval(id);
-                nyanCat.style.display = 'none';
-                nyanAudio.pause();
-                nyanAudio.currentTime = 0;
+                nyanCat.parentNode.removeChild(nyanCat);
+                nyanCount--;
+                finishedNyanCount++;
+                nyanCounter.getElementsByTagName('span')[0].textContent = finishedNyanCount;
+                if (nyanCount === 0) {
+                    nyanSong.pause();
+                    nyanSong.currentTime = 0;
+                    nyanCounter.style.opacity = 0;
+                }
             } else {
                 pos++;
                 nyanCat.style.left = pos + 'px';
             }
         }
     });
-});
+});    
